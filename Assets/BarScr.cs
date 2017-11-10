5264673da057101a636568016b7c956e47b19823
yscho529 <yscho529@gmail.com>
2017-11-10 18:32:54 -0500

    First Commit

---
+++ Assets/BarScr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarScr : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float speed;
+    private Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos = Input.mousePosition;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
+	}
+}
