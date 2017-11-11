d1990ce2855a8e5a19311ba1fc4aea5b819b9b07
yscho529 <yscho529@gmail.com>
2017-11-11 01:23:45 -0500

    walls n balls

---
+++ Assets/Scripts/BallScr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScr : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
