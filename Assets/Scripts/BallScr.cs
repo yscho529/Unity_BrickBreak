d60544db3310a70b5a9e38d0d526b78af513253a
yscho529 <yscho529@gmail.com>
2017-11-12 20:06:21 -0500

    11_12

--- Assets/Scripts/BallScr.cs
+++ Assets/Scripts/BallScr.cs
@@ -9,7 +9,7 @@ public class BallScr : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(0, -60);
+        rb.velocity = new Vector2(0, -100);
 	}
 	
 	// Update is called once per frame
